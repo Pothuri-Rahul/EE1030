023717675ed802a11de5cfa296cb44404669b963
Pothuri-Rahul <djrahulmaharaj@gmail.com>
2024-10-07 17:49:48 +0530

    commit

--- matgeo/matgeo2/codes/plot.py
+++ matgeo/matgeo2/codes/plot.py
@@ -33,10 +33,6 @@ ax.plot(x1, y1, color='orange')
 ax.plot(x2, y2, color='blue')
 
 # Define the intersection points
-# The intersection points of the circles can be found by solving the equations:
-# (x - 1)^2 + y^2 = 1 and x^2 + y^2 = 1
-# This gives the points (0.5, ±√(0.75))
-
 y_intersection = np.sqrt(0.75)
 intersection_points = [(0.5, y_intersection), (0.5, -y_intersection)]
 
@@ -47,6 +43,19 @@ x_fill2 = np.sqrt(1 - y_fill**2)      # From circle 2
 
 # Fill between the two curves in the intersection area
 ax.fill_betweenx(y_fill, x_fill1, x_fill2, where=(x_fill2 >= x_fill1), color='cyan', alpha=0.5)
+
+# Plot a solid line connecting the intersection points
+ax.plot([intersection_points[0][0], intersection_points[1][0]],
+        [intersection_points[0][1], intersection_points[1][1]], 
+        color='red', linewidth=2, label='Line joining intersections')
+
+# Plot the centers of the circles
+ax.plot(circle1_center[0], circle1_center[1], 'o', color='orange', markersize=8, label='Center 1 (1, 0)')
+ax.plot(circle2_center[0], circle2_center[1], 'o', color='blue', markersize=8, label='Center 2 (0, 0)')
+
+# Annotate the centers
+ax.text(circle1_center[0], circle1_center[1] + 0.1, '(1, 0)', fontsize=10, ha='center', color='orange')
+ax.text(circle2_center[0], circle2_center[1] + 0.1, '(0, 0)', fontsize=10, ha='center', color='blue')
 
 # Set axis limits and aspect ratio
 ax.set_xlim(-1.5, 2)
@@ -60,6 +69,9 @@ ax.set_title('Intersection of Two Circles')
 
 # Add grid for better visualization
 ax.grid(True)
+
+# Place the legend in the top right corner
+ax.legend(loc='upper right')
 
 # Save the plot as a PNG file
 plt.savefig('intersection_of_circles.png', dpi=300, bbox_inches='tight')
